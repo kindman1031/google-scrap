47aa8310cef360f52f1d0a0debdd2747b134d949
vijayant <vijayant.saini123@gmail.com>
2016-05-03 14:35:26 +0530

    added fuzzy search to refine results

--- scrap.py
+++ scrap.py
@@ -2,22 +2,37 @@
 
 import requests
 from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
 
 
 
-
-def google(query):
+def google(query, cleaned=1):
     url = "https://www.google.co.in/search?q=" + query
     r = requests.get(url, headers={"Accept" : "*/*", "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"})
     if r.status_code == 200:
-        print r.request.headers
-        return BeautifulSoup(r.text)
+        res = parseSoup(BeautifulSoup(r.text), query)
+        if cleaned == 1:
+            cleanedResults = []
+            for x in res:
+                if x["fuzz"] > 50 and x["text"] != "Cached":
+                    cleanedResults.append(x)
+            return cleanedResults
+        return res
 
-def parseSoup(s):
+def parseSoup(s, query):
+    result = []
     for tag in s.find_all('a'):
-        print tag
+        try:
+            st = str(tag.string)
+            href = str(tag['href'])
+            ratio = fuzz.partial_ratio(st.upper(), query.upper())
+            result.append({"fuzz": ratio, "host": href[7 : int(href.find('/', 8))],  "href": href, "text": st})
+        except KeyError, AttributeError:
+            continue
+    return result
 
 
-soup = google("chete karda mp3")
-parseSoup(soup)
+r = google("chete karda mp3")
+for x in r:
+    print x["fuzz"], x["text"], x["href"] 
 #print soup.prettify()
