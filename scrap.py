86483e2c02c98b849b4d10a40940c41b69b7b733
vijayant <vijayant.saini123@gmail.com>
2016-12-03 00:52:34 +0530

    packaged

--- scrap.py
+++ scrap.py
@@ -5,10 +5,9 @@ from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz
 
 
-
-def google(query, cleaned=1):
+def google(query, cleaned=0):
     url = "https://www.google.co.in/search?q=" + query
-    r = requests.get(url, headers={"Accept" : "*/*", "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"})
+    r = requests.get(url, headers={"Accept" : "*/*", "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36 googlescrap/0.1.0"})
     if r.status_code == 200:
         res = parseSoup(BeautifulSoup(r.text), query)
         if cleaned == 1:
@@ -17,7 +16,7 @@ def google(query, cleaned=1):
                 if x["fuzz"] > 50 and x["text"] != "Cached":
                     cleanedResults.append(x)
             return cleanedResults
-        return res
+    return res
 
 def parseSoup(s, query):
     result = []
@@ -32,7 +31,7 @@ def parseSoup(s, query):
     return result
 
 if __name__ == "__main__":
-    r = google("chete karda mp3")
+    r = google("chete karda mp3", 1)
     for x in r:
         print x["fuzz"], x["text"], x["href"] 
 #print soup.prettify()
