993f18c799f1805d9d1ed4c19a27e05a6f2909d7
vijayant <vijayant.saini123@gmail.com>
2016-12-04 02:50:23 +0530

    release lock

--- google_scrap/scrap.py
+++ google_scrap/scrap.py
@@ -7,22 +7,30 @@ from fuzzywuzzy import fuzz
 
 def google(query, cleaned=0):
     url = "https://www.google.co.in/search?q=" + query
-    r = requests.get(url, headers={"Accept" : "*/*", "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36 googlescrap/0.1.0"})
-    if r.status_code == 200:
-        res = parseSoup(BeautifulSoup(r.text), query)
-        if cleaned == 1:
-            cleanedResults = []
-            for x in res:
-                if x["fuzz"] > 50 and x["text"] != "Cached":
-                    cleanedResults.append(x)
-            return cleanedResults.sort()
-    return res
+    try:
+        r = requests.get(url, headers={"Accept" : "*/*", "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36 googlescrap/0.1.0"})
+        if r.status_code == 200:
+            res = parseSoup(BeautifulSoup(r.text), query)
+            if cleaned == 1:
+                cleanedResults = []
+                for x in res:
+                    if x["fuzz"] >= 50 and x["text"] != "Cached":
+                        cleanedResults.append(x)
+
+            return cleanedResults
+
+        else:
+            return [r.status_code]
+
+    except Exception as e:
+        print e
+        quit()
 
 def parseSoup(s, query):
     result = []
     for tag in s.find_all('a'):
         try:
-            st = str(tag.string)
+            st = tag.text.encode('utf-8')
             href = str(tag['href'])
             ratio = fuzz.partial_ratio(st.upper(), query.upper())
             result.append({"fuzz": ratio, "host": href[7 : int(href.find('/', 8))],  "href": href, "text": st})
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-
 setup(
-        name='google_scrap',
-        version='0.1.0',
-        description='A python Google India Search scrapper for googing',
-        url='https;//github.com/vijayant123/google_scrap.git',
-        author='Vijayant Saini',
-        license='MIT',
-        packages=['google_scrap'],
-        zip_safe=False
-        )
+    name='google_scrap',
+    version='0.1.0',
+    description='A python Google India Search scrapper for googing',
+    url='https://github.com/vijayant123/google_scrap',
+    author='Vijayant Saini',
+    author_email='vijayant.saini123@gmail.com',
+    license='MIT',
+    packages=['google_scrap'],
+    zip_safe=False
+)
