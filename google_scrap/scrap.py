91161241bf2666e55f3d5614a67e6daab08ac989
vijayant <vijayant.saini123@gmail.com>
2016-12-06 02:19:15 +0530

    fixes

--- google_scrap/scrap.py
+++ google_scrap/scrap.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 
 import requests
 from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz
+from urlparse import urlparse
 
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def google(query, cleaned=0):
     url = "https://www.google.co.in/search?q=" + query
     try:
+        _url = urlparse(url)
+        host = _url.scheme + '://' + _url.netloc + '/'
         r = requests.get(url, headers={"Accept" : "*/*", "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36 googlescrap/0.1.0"})
         if r.status_code == 200:
-            res = parseSoup(BeautifulSoup(r.text), query)
+            res = parseSoup(BeautifulSoup(r.text), query, host)
             if cleaned == 1:
                 cleanedResults = []
                 for x in res:
@@ -26,14 +33,18 @@ def google(query, cleaned=0):
         print e
         quit()
 
-def parseSoup(s, query):
+def parseSoup(s, query, host):
     result = []
     for tag in s.find_all('a'):
         try:
             st = tag.text.encode('utf-8')
             href = str(tag['href'])
+
+            if not href.startswith('http'):
+                href = host + href
+
             ratio = fuzz.partial_ratio(st.upper(), query.upper())
-            result.append({"fuzz": ratio, "host": href[7 : int(href.find('/', 8))],  "href": href, "text": st})
+            result.append({"fuzz": ratio, "host": href[8 : int(href.find('/', 8))],  "href": href, "text": st})
         except KeyError, AttributeError:
             continue
     return result
